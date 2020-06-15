2dbfa36663425086e320227dbfe2da51f8f465f5
deepBrainWH <673602165@qq.com>
2020-06-15 23:33:38 +0800

    classification and reinforcement learning

--- torch_cifar10_classification.py
+++ torch_cifar10_classification.py
@@ -1,24 +1,27 @@
 import torch.nn as nn
 import torch
+import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torchvision
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.optim as optim
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root="./data", train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+train_set = torchvision.datasets.CIFAR10(root="./data", train=True,
+                                         download=False, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=4,
+                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root="./data", train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+test_set = torchvision.datasets.CIFAR10(root="./data", train=False,
+                                        download=False, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=4,
+                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 
 def imshow(img):
     img = img / 2 + 0.5
@@ -28,7 +31,57 @@ def imshow(img):
 
 
 # get some random training images.
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-imshow(torchvision.utils.make_grid(images))
-print(" ".join('%5s' % classes[labels[j]] for j in range(4)))
+# dataiter = iter(train_loader)
+# images, labels = dataiter.next()
+# imshow(torchvision.utils.make_grid(images))
+# print(" ".join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+# Training network
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 1000 == 0:
+            print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 1000))
+            running_loss = 0.0
+print("Training finished!")
+
+# if __name__ == '__main__':
+#     a = torch.rand(11, 11).reshape((1, 1, 11, 11))
+#     print(a.shape)
+#     # 对于conv的输入(
+#     res = nn.Conv2d(1, 1, 3)(a)
+#     # print(a.size())
+#     print(res.shape)
